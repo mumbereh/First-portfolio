0e65d62bd3e0884f92c5bdcb30767268ba26c18e
mumbereh <siirasonh@gmail.com>
2023-04-18 16:12:29 +0300

    Modified style.css file

--- index.js
+++ index.js
@@ -2,16 +2,18 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav');
 const closeBtn = document.querySelector('.close-button');
-
+const links = document.querySelectorAll('.links');
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('show');
   menuToggle.style.display = 'none';
   closeBtn.style.display = 'block';
 });
-
 closeBtn.addEventListener('click', () => {
   nav.classList.remove('show');
   closeBtn.style.display = 'none';
   menuToggle.style.display = 'block';
 });
-
+links.forEach((n) => n.addEventListener('click', () => {
+  nav.classList.remove('show');
+  menuToggle.style.display = 'block';
+}));
