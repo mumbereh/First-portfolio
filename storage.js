2cbffb7cf56a4ab449b7805debe2e7000d878140
mumbereh <siirasonh@gmail.com>
2023-04-22 22:29:07 +0300

    Add storage functionality the form

---
+++ storage.js
@@ -0,0 +1,25 @@
+const formInputs = document.querySelectorAll('.capture-data');
+const formInputsArr = Array.from(formInputs);
+const formStorage = {
+  name: '',
+  email: '',
+  message: '',
+};
+function loadLocalStorage() {
+  const formStr = localStorage.getItem('formData');
+  if (formStr) {
+    const formData = JSON.parse(formStr);
+    formInputsArr.forEach((formInput) => {
+      formInput.value = formData[formInput.attributes.name.value];
+    });
+  }
+}
+formInputsArr.forEach((formInput) => {
+  formInput.addEventListener('keyup', (e) => {
+    const input = e.target.attributes.name.value;
+    formStorage[input] = e.target.value;
+    const formStorageStr = JSON.stringify(formStorage);
+    localStorage.setItem('formData', formStorageStr);
+  });
+});
+window.addEventListener('load', loadLocalStorage);
